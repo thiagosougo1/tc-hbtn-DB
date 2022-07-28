712287594e851bb5197438e21c7d96cecc9d835e
thiagosougo1 <thiagosg@ciandt.com>
2022-07-27 22:25:12 -0300

    EX 0

---
+++ 0x02/java_jdbc_manipulation/src/Cliente.java
@@ -0,0 +1,55 @@
+public class Cliente {
+    private int id;
+    private String nome;
+    private int idade;
+    private String cpf;
+    private String rg;
+
+    public Cliente(int id, String nome, int idade, String cpf, String rg) {
+        setId(id);
+        setNome(nome);
+        setIdade(idade);
+        setCpf(cpf);
+        setRg(rg);
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public int getIdade() {
+        return idade;
+    }
+
+    public void setIdade(int idade) {
+        this.idade = idade;
+    }
+
+    public String getCpf() {
+        return cpf;
+    }
+
+    public void setCpf(String cpf) {
+        this.cpf = cpf;
+    }
+
+    public String getRg() {
+        return rg;
+    }
+
+    public void setRg(String rg) {
+        this.rg = rg;
+    }
+}
