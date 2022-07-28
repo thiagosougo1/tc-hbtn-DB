b99b56a527d70b3ca5c659a54b6922cc659150fa
thiagosougo1 <thiagosg@ciandt.com>
2022-07-28 20:34:34 -0300

    Ex 0

---
+++ jpa_hibernate/src/main/java/entities/Pessoa.java
@@ -0,0 +1,20 @@
+package entities;
+
+@Entity
+@Table(name="pessoa")
+public class Pessoa {
+    @Id
+    private int id;
+
+    @Column
+    private String name;
+
+    @Column
+    private String email;
+
+    @Column
+    private String cpf;
+
+    @Column
+    private String dataDeNascimento;
+}
---
+++ jpa_hibernate/src/main/java/entities/Produto.java
@@ -0,0 +1,21 @@
+package entities;
+
+@Entity
+@Table(name="produto")
+public class Produto {
+
+    @Id
+    private int id;
+
+    @Column
+    private String nome;
+
+    @Column
+    private int quantidade;
+
+    @Column
+    private double preco;
+
+    @Column
+    private String status;
+}
