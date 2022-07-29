6cb993fc26938f25167a6358543eed1fd0c61eb7
thiagosougo1 <thiagosg@ciandt.com>
2022-07-29 11:39:21 -0300

    EX 0

---
+++ java_jpa_hibernate_mapeamento/gestao-cursos-api/src/main/java/models/AlunoModel.java
@@ -0,0 +1,44 @@
+package models;
+
+import entities.Aluno;
+import javax.persistence.*;
+
+public class AlunoModel {
+
+    public void create(Aluno aluno) {
+        EntityManagerFactory emf = Persistence.createEntityManagerFactory("gestao-cursos-jpa");
+        EntityManager em = emf.createEntityManager();
+
+        try {
+            System.out.println("Iniciando a transação");
+            em.getTransaction().begin();
+            em.persist(aluno);
+            em.getTransaction().commit();
+            System.out.println("Aluno criado com sucesso !!!");
+        } catch (Exception e) {
+            em.close();
+            System.err.println("Erro ao criar um aluno !!!" + e.getMessage());
+        } finally {
+            em.close();
+            System.out.println("Finalizando a transação");
+        }
+    }
+
+    public Aluno findById(Long id) {
+        // TODO
+        return null;
+    }
+
+    public  List<Aluno> findAll() {
+        // TODO
+        return null;
+    }
+
+    public void update(Aluno aluno) {
+        // TODO
+    }
+
+    public void delete(Aluno aluno) {
+        // TODO
+    }
+}
